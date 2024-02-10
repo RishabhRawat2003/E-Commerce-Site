f9eaebd69e649ddf777f00bf4feb7eec596c2edc
RishabhRawat2003 <rawatrishabh953@gmail.com>
2024-02-10 13:06:03 +0530

    Category Done For Small Devices

--- main/project.js
+++ main/project.js
@@ -1,36 +1,36 @@
 const allProducts = async () => {
 
-    const response = await fetch('https://dummyjson.com/products?limit=100')
-    const result = await response.json()
-    return result
+  const response = await fetch('https://dummyjson.com/products?limit=100')
+  const result = await response.json()
+  return result
 
 }
 
 const singleProductById = async (id) => {
-    const response = await fetch(`https://dummyjson.com/products/${id}`)
-    const result = await response.json()
-    return result
+  const response = await fetch(`https://dummyjson.com/products/${id}`)
+  const result = await response.json()
+  return result
 }
 
 const searchProducts = async (search) => {
 
-    const response = await fetch(`https://dummyjson.com/products/search?q=${search}`)
-    const result = await response.json()
-    return result
+  const response = await fetch(`https://dummyjson.com/products/search?q=${search}`)
+  const result = await response.json()
+  return result
 }
 
 const allProductsCategories = async () => {
 
-    const response = await fetch('https://dummyjson.com/products/categories')
-    const result = await response.json()
-    return result
+  const response = await fetch('https://dummyjson.com/products/categories')
+  const result = await response.json()
+  return result
 }
 
 const productsOfCategories = async (category) => {
 
-    const response = await fetch(`https://dummyjson.com/products/category/${category}`)
-    const result = await response.json()
-    return result
+  const response = await fetch(`https://dummyjson.com/products/category/${category}`)
+  const result = await response.json()
+  return result
 }
 
 
@@ -39,12 +39,14 @@ let all_Products = allProducts()
 
 let single_Product = singleProductById()
 //console.log(single_Product)
+
 let search_products = searchProducts()
 //console.log(search_products)
+
 let all_Categories = allProductsCategories()
 //console.log(all_Categories)
 
-let products_Categories = productsOfCategories('tops')
+let products_Categories = productsOfCategories('Smartphones')
 //console.log(products_Categories)
 
 
@@ -61,82 +63,128 @@ const sidebar = document.querySelector('.sidebar')
 
 
 icon.addEventListener('click', function (e) {
-    e.preventDefault()
-    if (icon.classList.contains('fa-bars')) {
-        icon.classList.toggle('fa-xmark')
-        sidebar.classList.toggle('right-0')
-    }
+  e.preventDefault()
+  if (icon.classList.contains('fa-bars')) {
+    icon.classList.toggle('fa-xmark')
+    sidebar.classList.toggle('right-0')
+  }
 })
 
 category_li.addEventListener('click', function (e) {
-    e.preventDefault()
-    category_li.classList.toggle('text-black')
-    category_Icon.classList.toggle('fa-rotate-180')
-    category_Dropdown.classList.toggle('md:top-16')
-    category_Dropdown.classList.toggle('xl:top-[8vh]')
-    category_Dropdown.classList.toggle('2xl:top-16')
+  e.preventDefault()
+  category_li.classList.toggle('text-black')
+  category_Icon.classList.toggle('fa-rotate-180')
+  category_Dropdown.classList.toggle('md:top-16')
+  category_Dropdown.classList.toggle('xl:top-[8vh]')
+  category_Dropdown.classList.toggle('2xl:top-16')
 
 })
 
 
 
 all_Categories.then((val) => {
-    const all_Catg = val
-    all_Catg.map((items) => {
-        function capitalizeFirstLetter(string) {
-            return string.charAt(0).toUpperCase() + string.slice(1);
-        }
-        const all_items = capitalizeFirstLetter(items)
-        categories_Items(all_items)
-    })
+  const all_Catg = val
+  all_Catg.map((items) => {
+    function capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    const all_items = capitalizeFirstLetter(items)
+    categories_Items(all_items)
+  })
 })
 
 function categories_Items(items) {
-    const span = document.createElement('span')
-    span.innerHTML = items
-    span.setAttribute('class', 'text-white cursor-pointer font-semibold hover:text-black hover:underline underline-offset-2')
-    category_Dropdown.appendChild(span)
+  const span = document.createElement('span')
+  span.innerHTML = items
+  span.setAttribute('class', 'text-white cursor-pointer font-semibold hover:text-black hover:underline underline-offset-2')
+  category_Dropdown.appendChild(span)
 }
 
 
 
 //body Functionality
-const thumbnail_Div = document.querySelector('.thumbnail')
-const slides = document.querySelector('.slides');
-const bulletTracer = document.querySelector('.bullet-tracer');
-const images = document.querySelectorAll('.thumbnail img');
-/*
 
-let index = 0;
+function autoSliderSmallDevices() {
+  const slides = document.querySelector('.slides');
+  const bulletTracer = document.querySelector('.bullet-tracer');
+  const images = document.querySelectorAll('.slides img');
 
-images.forEach((_, i) => {
-  const bullet = document.createElement('div');
-  bullet.setAttribute('class','bullet w-2 h-2 bg-white rounded-full cursor-pointer mx-1')
-  bulletTracer.appendChild(bullet);
-  bullet.addEventListener('click', () => {
-    index = i;
+
+  let index = 0;
+
+  images.forEach((_, i) => {
+    const bullet = document.createElement('div');
+    bullet.setAttribute('class', 'bullet w-2 h-2 bg-white rounded-full cursor-pointer mx-1')
+    bulletTracer.appendChild(bullet);
+    bullet.addEventListener('click', () => {
+      index = i;
+      updateSlider();
+    });
+  });
+
+
+  function updateSlider() {
+    slides.style.transform = `translateX(${-index * 100}%)`;
+    const bullets = document.querySelectorAll('.bullet');
+    bullets.forEach((bullet, i) => {
+      if (i === index) {
+        bullet.setAttribute('class', 'bullet w-2 h-2 bg-gray-700 rounded-full cursor-pointer mx-1')
+      } else {
+        bullet.setAttribute('class', 'bullet w-2 h-2 bg-white rounded-full cursor-pointer mx-1')
+      }
+    });
+  }
+
+  function nextSlide() {
+    index++;
+    if (index === images.length) index = 0;
     updateSlider();
-  });
-});
+  }
 
+  setInterval(nextSlide, 3000);
 
-function updateSlider() {
-  slides.style.transform = `translateX(${-index * 100}%)`;
-  const bullets = document.querySelectorAll('.bullet');
-  bullets.forEach((bullet, i) => {
-    if (i === index) {
-      bullet.setAttribute('class' ,'bullet w-2 h-2 bg-gray-700 rounded-full cursor-pointer mx-1')
-    } else {
-      bullet.setAttribute('class' ,'bullet w-2 h-2 bg-white rounded-full cursor-pointer mx-1')
-    }
-  });
 }
 
-function nextSlide() {
-  index++;
-  if (index === images.length) index = 0;
-  updateSlider();
+function autoSliderMediumDevices() {
+  const slides2 = document.querySelector('.slides2');
+  const bulletTracer2 = document.querySelector('.bullet-tracer2');
+  const images2 = document.querySelectorAll('.slides2 img');
+
+
+  let index1 = 0;
+
+  images2.forEach((_, i) => {
+    const bullet2 = document.createElement('div');
+    bullet2.setAttribute('class', 'bullet2 w-2 h-2 bg-white rounded-full cursor-pointer mx-1')
+    bulletTracer2.appendChild(bullet2);
+    bullet2.addEventListener('click', () => {
+      index1 = i;
+      updateSlider2();
+    });
+  });
+
+
+  function updateSlider2() {
+    slides2.style.transform = `translateX(${-index1 * 100}%)`;
+    const bullets = document.querySelectorAll('.bullet2');
+    bullets.forEach((bullet, i) => {
+      if (i === index1) {
+        bullet.setAttribute('class', 'bullet2 w-2 h-2 bg-gray-700 rounded-full cursor-pointer mx-1')
+      } else {
+        bullet.setAttribute('class', 'bullet2 w-2 h-2 bg-white rounded-full cursor-pointer mx-1')
+      }
+    });
+  }
+
+  function nextSlide2() {
+    index1++;
+    if (index1 === images2.length) index1 = 0;
+    updateSlider2();
+  }
+
+  setInterval(nextSlide2, 3000);
+
 }
 
-setInterval(nextSlide, 3000); 
-*/
+autoSliderSmallDevices()
+autoSliderMediumDevices() 
